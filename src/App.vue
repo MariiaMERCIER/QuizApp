671389e3ac6dbb5fd703ee2f9373d07ce63cadbd
Mariia IVANCHENKO <mariia.mercier@outlook.fr>
2023-01-31 11:27:21 +0100

    json

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,32 @@
+<script setup>
+import data from "./data/quizdata.json";
+import { ref, watch } from "vue";
+
+const quizes = ref(data);
+const search = ref("");
+
+watch(search, () => {
+  quizes.value = data.filter((quiz) =>
+    quiz.name.toLowerCase().includes(search.value.toLowerCase())
+  );
+});
+</script>
+
 <template>
   <div class="container">
     <header>
       <h1>Quizes</h1>
-      <input type="text" />
+      <input v-model.trim="search" type="text" placeholder="Search..." />
     </header>
-    <div class="options-container"></div>
+    <div class="options-container">
+      <div v-for="quiz in quizes" class="card">
+        <img :src="quiz.img" alt="material" />
+        <div class="card-text">
+          <h2>{{ quiz.name }}</h2>
+          <p>{{ quiz.questions.length }} questions</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -23,6 +45,7 @@ header {
 header h1 {
   font-weight: bold;
   margin-right: 30px;
+  font-family: Arial, Helvetica, sans-serif;
 }
 
 header input {
@@ -30,5 +53,38 @@ header input {
   background-color: rgba(128, 128, 128, 0.1);
   padding: 10px;
   border-radius: 5px;
+}
+
+.options-container {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 40px;
+}
+
+/* CARD */
+.card {
+  width: 310px;
+  overflow: hidden;
+  border-radius: 2%;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+  margin-bottom: 35px;
+  margin-right: 20px;
+  cursor: pointer;
+}
+
+.card img {
+  width: 100%;
+  height: 190px;
+  margin: 0;
+}
+
+.card .card-text {
+  font-family: Arial, Helvetica, sans-serif;
+  padding: 0 5px;
+}
+
+.card .card-text h2 {
+  font-family: Arial, Helvetica, sans-serif;
+  font-weight: bold;
 }
 </style>
